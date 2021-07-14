75dd0245d4acbb1cb8d508218fb896f6b55eb587
syuheifujita <syuheifujita0117@gmail.com>
2021-07-14 10:11:13 +0900

    コンテンツを取得

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -33,5 +39,17 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.API_KEY,
+        serviceId: 'gatsby-media',
+        apis: [
+          {
+            endpoint: "blog",
+          },
+        ],
+      },
+    },
   ],
 }
